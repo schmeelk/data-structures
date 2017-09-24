5bbf316dc34690ba0feeb422101c821269220139
schmeelk <schmeels@newschool.edu>
2017-09-24 12:27:56 -0400

    Week4 Tasks

---
+++ Weekly-Assignments/Week04/PartTwo/Task4.js
@@ -0,0 +1,27 @@
+// npm install mongodb
+
+var request = require('request');
+// IN MONGO exists a database `aa` with a collection `meetings`
+var dbName = 'aa'; // name of Mongo database (created in the Mongo shell)
+var collName = 'meetings'; // name of Mongo collection (created in the Mongo shell)
+
+// Request the JSON data on aa meetings 
+// Insert the list of aa meetings (contained in an array) in the Mongo collection
+request('https://raw.githubusercontent.com/schmeelk/data-structures/master/Weekly-Assignments/Week03/meetingsData.txt', function(error, response, body) {
+    var meetingData = JSON.parse(body);
+    // Connection URL
+    var url = 'mongodb://' + process.env.IP + ':27017/' + dbName;
+    // Retrieve
+    var MongoClient = require('mongodb').MongoClient; 
+    MongoClient.connect(url, function(err, db) {
+        if (err) {return console.dir(err);}
+
+        var collection = db.collection(collName);
+
+        // THIS IS WHERE THE DOCUMENT(S) IS/ARE INSERTED TO MONGO:
+        meetingData.forEach(function(element) {
+            collection.insert(element);
+        });
+        db.close();
+    }); //MongoClient.connect
+}); //request
